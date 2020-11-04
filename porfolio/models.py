5a8daf2e8e38dd06722913b1d33c46774cfec4d4
owaiss007 <owais1998@live.com>
2020-11-04 13:54:57 +0500

    Added First Project

--- porfolio/models.py
+++ porfolio/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Project(models.Model):
     titlehead = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='portfolio/images/')
+    image = models.ImageField(upload_to='porfolio/images/')
     url = models.URLField(blank=True)
