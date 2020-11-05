01739014d2ca48d914a755d8deb3f54c352135ef
owaiss007 <owais1998@live.com>
2020-11-05 10:32:27 +0500

    Added blogs

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
 def all_blogs(request):
-    return render(request, template_name='blog/all_blogs.html')
+    blogs = Blog.objects.all()
+    return render(request, template_name='blog/all_blogs.html', context={'blogs':blogs})
