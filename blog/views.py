9b1b2340f914ce96c0c4aa63c0f6050e3424d59d
owaiss007 <owais1998@live.com>
2020-11-05 13:50:15 +0500

    Finito

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blogs(request):
     blogs = Blog.objects.all()
     return render(request, template_name='blog/all_blogs.html', context={'blogs':blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, template_name='blog/detail.html', context={'blog':blog})
